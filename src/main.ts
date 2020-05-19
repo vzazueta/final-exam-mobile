4a2e0bd48f30d410d89cc9d74b83a0e708ac99fe
Ivan Morett <luisivanmorett@gmail.com>
2020-05-19 12:52:12 -0500

    fix problem with heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
