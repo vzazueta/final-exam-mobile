18940939b7d55db97f891d6f066aa22bbaac5e1d
Iván Morett <luisivanmorett@gmail.com>
2020-06-01 18:28:09 -0500

    real bottom navigation

--- app/src/main/java/com/example/final_exam/MainActivity.java
+++ app/src/main/java/com/example/final_exam/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.final_exam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.core.content.ContextCompat;
@@ -7,15 +8,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    ImageView addCitaMenu, listaCitaMenu, mapMenu;
-    View.OnClickListener menuClickListener = view -> {
-        int idIconMenu = view.getId();
-        setSelected(idIconMenu);
-    };
+    BottomNavigationView menu;
 
 
     @Override
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setViewComponents();
-        setTint( R.id.iv_list_cita_icon, R.color.red_accent_3);
+
         if (findViewById(R.id.fragment_container_menu) != null) {
             if (savedInstanceState != null) return;
             // Create a new Fragment to be placed in the activity layout
@@ -40,32 +40,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setViewComponents(){
-        addCitaMenu = findViewById(R.id.iv_create_icon);
-        listaCitaMenu = findViewById(R.id.iv_list_cita_icon);
-        mapMenu = findViewById(R.id.iv_map_icon);
-        mapMenu.setOnClickListener(menuClickListener);
-        addCitaMenu.setOnClickListener(menuClickListener);
-        listaCitaMenu.setOnClickListener(menuClickListener);
+        menu = findViewById(R.id.bottom_navigation);
+        menu.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.page_1:
+                            replaceFragment( new ListaCitasFragment());
+                            return true;
+                        case R.id.page_2:
+                            replaceFragment( new MapFragment());
+                            return true;
+                        case R.id.page_3:
+                            replaceFragment( new CreateCitaFragment());
+                            return true;
+                    }
+                    return false;
+                }
+            });
     }
 
-    public void setSelected(int i) {
-        setTint( R.id.iv_map_icon,  R.color.white);
-        setTint( R.id.iv_create_icon,   R.color.white);
-        setTint( R.id.iv_list_cita_icon,   R.color.white);
-        setTint( i,  R.color.red_accent_3);
-        switch (i) {
-            case R.id.iv_map_icon:
-                replaceFragment( new MapFragment());
-                break;
-            case R.id.iv_create_icon:
-                replaceFragment( new CreateCitaFragment());
-                break;
-            case R.id.iv_list_cita_icon:
-            default:
-                replaceFragment( new ListaCitasFragment());
-                break;
-        }
-    }
 
     public void setTint(int id, int color){
         ((ImageView)(findViewById(id))).setColorFilter(ContextCompat.getColor(this, color));
